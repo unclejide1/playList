e6447e01f2d25ba250155fa8b49fe755a1747f9b
Jideofor Nwoke <njusticej@gmail.com>
2020-01-14 01:32:11 +0100

    Initial commit

---
+++ src/com/decagonhq/Main.java
@@ -0,0 +1,9 @@
+package com.decagonhq;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Album album = new Album("Stormbringer", "Deep purple");
+	album.addSong("StormBringer", 4.6)
+    }
+}
